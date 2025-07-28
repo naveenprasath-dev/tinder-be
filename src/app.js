af8013054a694b7fc43d6c38f88e40806c0ec504
naveenprasath-dev <naveenprasath.dev@gmail.com>
2025-07-28 19:07:17 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,45 +2,36 @@ const express = require("express");
 
 const app = express();
 
-// This over-ride's all the routes.
-// Put this at last.
-// app.use("/",(req, res) => {
-//     res.send("Hello");
-// })
+app.get(
+    "/user",
+    (req, res, next) => {
+      console.log("Handling the route user!!");
+      next();
+    },
+    (req, res, next) => {
+      console.log("Handling the route user 2!!");
+      // res.send("2nd Response!!");
+      next();
+    },
+  
+    (req, res, next) => {
+      console.log("Handling the route user 3!!");
+      // res.send("3rd Response!!");
+      next();
+    },
+    (req, res, next) => {
+      console.log("Handling the route user 4!!");
+      // res.send("4th Response!!");
+      next();
+    },
+    (req, res, next) => {
+      console.log("Handling the route user 5!!");
+      res.send("5th Response!!");
+    }
+  );
+  
 
 
-app.get("/abc", (req,res) => {
-    res.send("Naveenprasath");
-});
-
-// Only GET call
-app.get("/user/:userId/:firstName/:lastName", (req,res) => {
-    console.log(req.query);
-    console.log(req.params);
-
-    res.send("Naveenprasath");
-})
-
-app.post("/user", (req,res) => {
-    res.send("Data Saved");
-})
-/**
- * The route will be on order of line written.
- * Order of route matters.
- * 
- */
-// request handler
-// This will match all the http method api calls.
-app.use("/test",(req, res) => {
-    res.send("Hello Test");
-})
-
-
-
-
-app.use("/",(req, res) => {
-    res.send("Hello");
-})
 app.listen(3001, () => {
     console.log("server is listening");
 });
