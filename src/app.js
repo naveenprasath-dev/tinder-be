9bc2550d64c3db64816f2f0ca8c8165388a0c33d
naveenprasath-dev <naveenprasath.dev@gmail.com>
2025-07-29 23:34:20 +0530

    CRUD API

--- src/app.js
+++ src/app.js
@@ -1,23 +1,29 @@
 const express = require("express");
 const connectDB = require("../src/config/database");
 const app = express();
+
 const User = require("../src/models/user");
+
+
+// Middlewares
+
+/**
+ * Reads the json data
+ * Converts the json data to javascript body
+ * Pass back to request
+ */
+app.use(express.json());
 
 
 // API 
 
+/**
+ * SignUp API
+ * 
+ */
 app.post("/signup", async (req, res) => {
-
-   
     // creating a new instance of the user Model.
-    const user = new User({
-        firstName: "Naveenprasath",
-        lastName: "selvam",
-        emailId:"test@gmail.com",
-        age:28,
-        gender:"Male",
-        password:"Naveen@123",
-    });
+    const user = new User(req.body);
 
     try {
         newUser = await user.save();
@@ -25,19 +31,86 @@ app.post("/signup", async (req, res) => {
     } catch (error) {
         res.status(500).send("Error adding user" + error.message);
     }
-   
-
   
 });
 
 
+/**
+ * Feed API - Get all data
+ * 
+ */
 
 
+app.get("/feed", async (req,res) => {
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (error) {
+        res.status(500).send("Error Fetching user from feed API" + error.message);
+    }
+
+});
 
 
+/**
+ * GET specific User
+ * 
+ * 
+ */
+
+app.get("/user", async (req,res) => {
+    try {
+
+        // More than one user
+        const users = await User.find({emailId:req.body.email});
+        
+        // one user only
+        // const users = await User.findOne({emailId:req.body.email});
+        res.send(users);
+    } catch (error) {
+        res.status(500).send("Error Fetching user from feed API" + error.message);
+    }
+
+});
 
 
+/**
+ * DELETE an user
+ * 
+ * 
+ */
 
+app.delete("/user", async (req,res) => {
+    try {
+        // More than one user
+        await User.findOneAndDelete({emailId:req.body.email});
+        
+        // one user only
+        // const users = await User.findOne({emailId:req.body.email});
+        res.send("deleted successfully");
+    } catch (error) {
+        res.status(500).send("Error Fetching user from feed API" + error.message);
+    }
+
+});
+
+
+/**
+ * PATCH API
+ * 
+ */
+app.patch("/user", async (req, res) => {
+    // creating a new instance of the user Model.
+    // const user = new User(req.body);
+
+    try {
+        newUser = await User.findByIdAndUpdate("6888ee6df382c9c52f4d514a", req.body, {returnDocument:"after"});
+        res.send(newUser);
+    } catch (error) {
+        res.status(500).send("Error adding user" + error.message);
+    }
+  
+});
 
 // DB connection
 connectDB()
