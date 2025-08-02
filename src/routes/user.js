7882d93b20e2d20281360f704b7c4d4b47807d3a
naveenprasath-dev <naveenprasath.dev@gmail.com>
2025-08-02 23:24:04 +0530

    feed API

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 const router = express.Router();
 
 
@@ -63,7 +64,59 @@ router.get("/user/requests/connections", userAuth, async (req, res) => {
     }
 });
 
+/**
+ * Get all the feed
+ * 
+ */
 
+router.get("/user/feed", userAuth, async (req, res)=> {
+    try {
+        // User should not see all the cards except.
+        // 0. his own card.
+        // 1.his connections.
+        // 2. his ignored list.
+        // 3. his sent connection list.
+        const loggedInUser = req.user;
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        const skip = (page-1)* limit;
+
+        if (limit > 100) {
+            limit = 5;
+        }
+        // Find all connection req, 
+        // either sent or received.
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                {fromUserId: loggedInUser._id},
+                {toUserId: loggedInUser._id},
+            ],
+        }).select("fromUserId toUserId");
+        // .populate("fromUserId", "firstName")
+        // .populate("toUserId", "firstName");
+
+        const hideUserFromFeed = new Set();
+        connectionRequests.forEach(element => {
+            hideUserFromFeed.add(element.fromUserId._id);
+            hideUserFromFeed.add(element.toUserId._id);
+
+        });
+
+        const users = await User.find({
+            $and: [
+                {_id: {$nin: Array.from(hideUserFromFeed)}},
+                {_id: {$ne: loggedInUser._id}}
+            ]
+           
+        }).select("firstName lastName age photoUrl")
+        .skip(skip).limit(limit);
+
+
+        res.status(200).send(users);
+    } catch (error) {
+        res.status(400).send("Error getting feed: " + error.message);
+    }
+});
 
 
 
