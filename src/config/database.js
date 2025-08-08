ca16bc76e5a5cc02effedc2d1c928d20697f756a
naveenprasath-dev <naveenprasath.dev@gmail.com>
2025-08-08 15:52:56 +0530

    updated password

--- src/config/database.js
+++ src/config/database.js
@@ -3,10 +3,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async() => {
     await mongoose.connect(
-        "mongodb+srv://naveenprasathdev:C8qMihgafc1yp1gB@devlopment-cluster.rnpze3f.mongodb.net/dev-tinder"
+        "mongodb+srv://naveenprasathdev:CMBnjaupkB8isJgn@devlopment-cluster.rnpze3f.mongodb.net/dev-tinder"
     );
 }
 
 module.exports = connectDB;
+
+
 
 
